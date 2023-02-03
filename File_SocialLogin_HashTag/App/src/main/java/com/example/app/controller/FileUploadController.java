b845a452956cfe621dd37c427a3a8470ce68892f
jyajoo <jyajoo1020@gmail.com>
2023-02-03 23:21:47 +0900

    feat: 파일 업로드

---
+++ File_SocialLogin_HashTag/App/src/main/java/com/example/app/controller/FileUploadController.java
@@ -0,0 +1,30 @@
+package com.example.app.controller;
+
+import java.io.File;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/upload")
+public class FileUploadController {
+
+  @Value("${custom.genFileDirPath}")
+  private String genFileDirPath;
+
+  @RequestMapping("")
+  @ResponseBody
+  public String upload(@RequestParam("img1") MultipartFile img1) {
+    File file = new File(genFileDirPath + "1.png");
+    try {
+      img1.transferTo(file);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return "업로드 완료";
+  }
+}
