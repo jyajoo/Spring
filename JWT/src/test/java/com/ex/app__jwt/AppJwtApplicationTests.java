eef96091d8e7c5e22520dccd2603226bfb8e152f
jyajoo <jyajoo1020@gmail.com>
2023-02-19 21:40:04 +0900

    settings

---
+++ JWT/src/test/java/com/ex/app__jwt/AppJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.app__jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
