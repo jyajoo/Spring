89e416e6545cabca8663bd96cb5c572e06fd953d
jyajoo <jyajoo1020@gmail.com>
2023-02-20 21:50:57 +0900

    test: JwtProvider로 SecretKey 객체 생성

--- JWT/src/test/java/com/ex/app__jwt/JWTTest.java
+++ JWT/src/test/java/com/ex/app__jwt/JWTTest.java
@@ -2,11 +2,13 @@ package com.ex.app__jwt;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.ex.app__jwt.jwt.JwtProvider;
 import io.jsonwebtoken.security.Keys;
 import java.util.Base64;
 import javax.crypto.SecretKey;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,6 +17,9 @@ class JWTTest {
 
 	@Value("${custom.jwt.secretKey}")
 	private String plainSecretKey;
+
+	@Autowired
+	private JwtProvider jwtProvider;
 
 	@Test
 	@DisplayName("secretKey 존재 확인")
@@ -32,6 +37,13 @@ class JWTTest {
 		String encodedKey = Base64.getEncoder().encodeToString(plainSecretKey.getBytes());
 		SecretKey secretKey = Keys.hmacShaKeyFor(encodedKey.getBytes());
 
+		assertThat(secretKey).isNotNull();
+	}
+
+	@Test
+	@DisplayName("JwtProvider 객체로 SecretKey 객체 생성")
+	void t3() {
+		SecretKey secretKey = jwtProvider.getSecretKey();
 		assertThat(secretKey).isNotNull();
 	}
 }
