bf14bcfe5432a038d147cb2a3634ff4eb5daed0e
jyajoo <jyajoo1020@gmail.com>
2023-02-20 21:58:25 +0900

    feat: SecretKey 캐싱

--- JWT/src/main/java/com/ex/app__jwt/jwt/JwtProvider.java
+++ JWT/src/main/java/com/ex/app__jwt/jwt/JwtProvider.java
@@ -9,11 +9,20 @@ import org.springframework.stereotype.Component;
 @Component   // 클래스 빈 등록
 public class JwtProvider {
 
+  private SecretKey cachedSecretKey;
   @Value("${custom.jwt.secretKey}")
   private String plainSecretKey;
 
-  public SecretKey getSecretKey() {
+  private SecretKey getPrivateSecretKey() {
     String encodedKey = Base64.getEncoder().encodeToString(plainSecretKey.getBytes());
     return Keys.hmacShaKeyFor(encodedKey.getBytes());
+  }
+
+  // private인 _getSecretKey 함수가 단 한 번만 실행되도록 함
+  public SecretKey getSecretKey() {
+    if (cachedSecretKey == null) {
+      cachedSecretKey = getPrivateSecretKey();
+    }
+    return cachedSecretKey;
   }
 }
