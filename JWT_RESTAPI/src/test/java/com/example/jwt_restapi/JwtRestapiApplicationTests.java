9f75d544729560eecd9e165417945b2a174a4ceb
jyajoo <jyajoo1020@gmail.com>
2023-02-23 16:56:57 +0900

    project setting

---
+++ JWT_RESTAPI/src/test/java/com/example/jwt_restapi/JwtRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRestapiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
