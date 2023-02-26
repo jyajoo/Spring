0b5d943927babd30523e46e88895556c38874a5a
jyajoo <jyajoo1020@gmail.com>
2023-02-26 21:54:01 +0900

    test: 응답 헤더에 JWT 확인

--- JWT_RESTAPI/src/test/java/com/example/jwt_restapi/JwtRestapiApplicationTests.java
+++ JWT_RESTAPI/src/test/java/com/example/jwt_restapi/JwtRestapiApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.jwt_restapi;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -24,10 +28,6 @@ class JwtRestapiApplicationTests {
   @Autowired
   private MockMvc mvc; // 요청과 응답을 의미하는 가짜 객체로 실제 서버에 배포하지 않고 테스트를 위한 라이브러리
 
-  /*
-  Post /member/login에 JSON 형태의 content를 담아 요청하면, 결과가 성공이어야 한다.
-
-   */
   @Test
   @DisplayName("POST /member/login 은 로그인 처리 URL")
   void t1() throws Exception {
@@ -43,5 +43,27 @@ class JwtRestapiApplicationTests {
     ).andDo(MockMvcResultHandlers.print());
 
     resultActions.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+  }
+
+  @Test
+  @DisplayName("POST /member/login의 ok 응답으로 JWT 발급")
+  void t2() throws Exception {
+    ResultActions resultActions = mvc.perform(
+        MockMvcRequestBuilders.post("/member/login")
+            .content("""
+                {
+                  "username" : "user1",
+                  "password" : "1234"
+                }
+                """.stripIndent())
+            .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+    ).andDo(MockMvcResultHandlers.print());
+
+    resultActions.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    MvcResult mvcResult = resultActions.andReturn();
+    MockHttpServletResponse response = mvcResult.getResponse();
+    String authentication = response.getHeader("Authentication");
+
+    assertThat(authentication).isNotEmpty();
   }
 }
