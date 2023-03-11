96cc531872e00870081774841361bc9058e541ea
jyajoo <jyajoo1020@gmail.com>
2023-03-11 17:44:04 +0900

    feat: username & password null 체크

--- JWT_RESTAPI/src/main/java/com/example/jwt_restapi/member/controller/MemberController.java
+++ JWT_RESTAPI/src/main/java/com/example/jwt_restapi/member/controller/MemberController.java
@@ -1,7 +1,7 @@
 package com.example.jwt_restapi.member.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +14,11 @@ public class MemberController {
 
   @PostMapping("/login")
   public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+
+    if (loginDto.isNotValid()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
     String body = "username : %s, password : %s".formatted(loginDto.getUsername(),
         loginDto.getPassword());
     return ResponseEntity.ok()
@@ -25,6 +30,11 @@ public class MemberController {
   public static class LoginDto {
     String username;
     String password;
+
+    public boolean isNotValid() {
+      return username == null || password == null || username.trim().length() == 0
+          || password.trim().length() == 0;
+    }
   }
 
 }
