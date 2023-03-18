6758e5a10c915b20949affabbb4eed0c379cf0e7
jyajoo <jyajoo1020@gmail.com>
2023-03-18 17:11:32 +0900

    feat: MemberContext 추가

---
+++ JWT_RESTAPI/src/main/java/com/example/jwt_restapi/member/dto/MemberContext.java
@@ -0,0 +1,44 @@
+package com.example.jwt_restapi.member.dto;
+
+import com.example.jwt_restapi.member.entity.Member;
+import com.example.jwt_restapi.member.entity.MemberRole;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+/*
+equals :  두 객체의 내용이 같은지, 동등성(equality) 비교
+hashCode : 두 객체가 같은 객체인지, 동일성(identity) 비교
+ */
+@Getter
+@EqualsAndHashCode(callSuper=false) // callSuper=false : 부모 클래스 고려 X
+public class MemberContext extends User {
+
+  private final Long id;
+  private final String username;
+  private final String email;
+  private final LocalDateTime createdDate;
+  private final LocalDateTime updatedDate;
+  private final Set<MemberRole> roleList;
+
+  public MemberContext(Member member) {
+    super(member.getUsername(), member.getPassword(), authorities(member.getRoleList()));
+    id = member.getId();
+    username = member.getUsername();
+    email = member.getEmail();
+    createdDate = member.getCreatedDate();
+    updatedDate = member.getUpdatedDate();
+    roleList = member.getRoleList();
+  }
+
+  private static Collection<? extends GrantedAuthority> authorities(Set<MemberRole> roleList) {
+    return roleList.stream().map(role -> new SimpleGrantedAuthority(role.name()))
+        .collect(Collectors.toList());
+  }
+}
