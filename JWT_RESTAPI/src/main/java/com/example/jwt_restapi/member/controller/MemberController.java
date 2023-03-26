2fa378da7a2e8997f4ba1700cf8d7dc82b32d4ed
jyajoo <jyajoo1020@gmail.com>
2023-03-26 23:53:49 +0900

    refactor: MemberDto 생성 및 service로 메서드 이동

--- JWT_RESTAPI/src/main/java/com/example/jwt_restapi/member/controller/MemberController.java
+++ JWT_RESTAPI/src/main/java/com/example/jwt_restapi/member/controller/MemberController.java
@@ -4,13 +4,11 @@ import com.example.jwt_restapi.base.dto.ResultCode;
 import com.example.jwt_restapi.base.dto.RsData;
 import com.example.jwt_restapi.member.dto.LoginRequest;
 import com.example.jwt_restapi.member.dto.MemberContext;
-import com.example.jwt_restapi.member.entity.Member;
+import com.example.jwt_restapi.member.dto.MemberDto;
 import com.example.jwt_restapi.member.service.MemberService;
-import com.example.jwt_restapi.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,26 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
   private final MemberService memberService;
-  private final PasswordEncoder passwordEncoder;
-  private final JwtProvider jwtProvider;
 
   @PostMapping("/login")
   public ResponseEntity<RsData<String>> login(@RequestBody LoginRequest loginRequest) {
 
-    if (loginRequest.isNotValid()) {
-      return ResponseEntity.badRequest()
-          .body(RsData.of(ResultCode.FAIL, "아이디 또는 비밀번호를 입력해주세요.", null));
-    }
-
-    Member member = memberService.findMemberByUsername(loginRequest.getUsername()).orElse(null);
-
-    if (member == null || !passwordEncoder.matches(loginRequest.getPassword(),
-        member.getPassword())) {
-      return ResponseEntity.badRequest()
-          .body(RsData.of(ResultCode.FAIL, "아이디 또는 비밀번호가 옳지 않습니다.", null));
-    }
-
-    String accessToken = jwtProvider.generateAccessKey(member);
+    String accessToken = memberService.login(loginRequest);
 
     return ResponseEntity.ok()
         .header("AccessToken", accessToken)
@@ -50,9 +33,18 @@ public class MemberController {
   }
 
   @GetMapping("/me")
-  public ResponseEntity<RsData<MemberContext>> findMember(
+  public ResponseEntity<RsData<MemberDto>> findMember(
       @AuthenticationPrincipal MemberContext memberContext) {
+
+    MemberDto memberDto = MemberDto.builder()
+        .id(memberContext.getId())
+        .username(memberContext.getUsername())
+        .email(memberContext.getEmail())
+        .roleSet(memberContext.getRoleSet())
+        .createdDate(memberContext.getCreatedDate())
+        .updatedDate(memberContext.getUpdatedDate())
+        .build();
     return ResponseEntity.ok()
-        .body(RsData.of(ResultCode.SUCCESS, "현재 로그인한 회원 정보", memberContext));
+        .body(RsData.of(ResultCode.SUCCESS, "현재 로그인한 회원 정보", memberDto));
   }
 }
--- JWT_RESTAPI/src/main/java/com/example/jwt_restapi/member/service/MemberService.java
+++ JWT_RESTAPI/src/main/java/com/example/jwt_restapi/member/service/MemberService.java
@@ -1,12 +1,15 @@
 package com.example.jwt_restapi.member.service;
 
+import com.example.jwt_restapi.base.exception.MemberException;
+import com.example.jwt_restapi.member.dto.LoginRequest;
 import com.example.jwt_restapi.member.entity.Member;
 import com.example.jwt_restapi.member.entity.MemberRole;
 import com.example.jwt_restapi.member.repository.MemberRepository;
+import com.example.jwt_restapi.security.jwt.JwtProvider;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +17,9 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
   private final MemberRepository memberRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtProvider jwtProvider;
+
 
   public Member join(String username, String password, String email) {
     Member member = Member.builder()
@@ -29,7 +35,18 @@ public class MemberService {
     return member;
   }
 
-  public Optional<Member> findMemberByUsername(String username) {
-    return memberRepository.findMemberByUsername(username);
+  public String login(LoginRequest loginRequest) {
+    if (loginRequest.isNotValid()) {
+      throw new MemberException("아이디 또는 비밀번호를 입력해주세요.");
+    }
+
+    Member member = memberRepository.findMemberByUsername(loginRequest.getUsername())
+        .orElseThrow(() -> new MemberException("회원이 존재하지 않습니다."));
+
+    if (member == null || !passwordEncoder.matches(loginRequest.getPassword(),
+        member.getPassword())) {
+      throw new MemberException("아이디 또는 비밀번호가 옳지 않습니다.");
+    }
+    return jwtProvider.generateAccessKey(member);
   }
 }
