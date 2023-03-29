b5712b017ea273cd244e20816c8a243c15a8c823
jyajoo <jyajoo1020@gmail.com>
2023-03-30 01:00:31 +0900

    feat: MethodArgumentNotValidException 핸들러 추가

--- JWT_RESTAPI/src/main/java/com/example/jwt_restapi/base/exception/ExceptionControllerAdvice.java
+++ JWT_RESTAPI/src/main/java/com/example/jwt_restapi/base/exception/ExceptionControllerAdvice.java
@@ -2,12 +2,36 @@ package com.example.jwt_restapi.base.exception;
 
 import com.example.jwt_restapi.base.dto.ResultCode;
 import com.example.jwt_restapi.base.dto.RsData;
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
+
+  @ExceptionHandler({MethodArgumentNotValidException.class})
+  public ResponseEntity<RsData<Object>> exceptionHandling(MethodArgumentNotValidException e) {
+
+    String msg = e
+        .getBindingResult()
+        .getAllErrors()
+        .stream()
+        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+        .collect(Collectors.joining(", "));
+
+    String data = e
+        .getBindingResult()
+        .getAllErrors()
+        .stream()
+        .map(DefaultMessageSourceResolvable::getCode)
+        .collect(Collectors.joining(", "));
+
+    return ResponseEntity.badRequest()
+        .body(RsData.of(ResultCode.FAIL, msg, data));
+  }
 
   @ExceptionHandler({MemberException.class, BoardException.class})
   public ResponseEntity<RsData<Object>> exceptionHandling(Exception e) {
