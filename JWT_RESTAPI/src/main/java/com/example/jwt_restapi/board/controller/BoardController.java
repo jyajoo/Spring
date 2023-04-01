6de83af7b8a1393f70067b34716cff8cbfa74e59
jyajoo <jyajoo1020@gmail.com>
2023-04-01 18:39:34 +0900

    chore: Operation, Parameter, Tag로 API 명세서 작성

--- JWT_RESTAPI/src/main/java/com/example/jwt_restapi/board/controller/BoardController.java
+++ JWT_RESTAPI/src/main/java/com/example/jwt_restapi/board/controller/BoardController.java
@@ -6,6 +6,9 @@ import com.example.jwt_restapi.board.dto.BoardDto;
 import com.example.jwt_restapi.board.dto.BoardUpdateRequest;
 import com.example.jwt_restapi.board.service.BoardService;
 import com.example.jwt_restapi.member.dto.MemberContext;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "BoardController", description = "게시물 조회 및 CRUD 기능을 제공하는 컨트롤러")
 public class BoardController {
 
   private final BoardService boardService;
 
   @GetMapping("/boards")
+  @Operation(summary = "게시물 전체 조회", description = "전체 게시물 목록을 제공합니다.")
   public ResponseEntity<RsData<List<BoardDto>>> getBoardList() {
     List<BoardDto> boardList = boardService.getBoardList();
 
@@ -33,23 +38,29 @@ public class BoardController {
   }
 
   @GetMapping("/board/{id}")
-  public ResponseEntity<RsData<BoardDto>> getBoard(@PathVariable Long id) {
+  @Operation(summary = "게시물 단건 조회", description = "하나의 게시물에 대한 정보를 제공합니다.")
+  public ResponseEntity<RsData<BoardDto>> getBoard(
+      @Parameter(description = "게시물 ID", required = true, example = "1") @PathVariable Long id) {
     BoardDto board = boardService.getBoard(id);
     return ResponseEntity.ok()
         .body(RsData.of(ResultCode.SUCCESS, "게시물 단건 조회", board));
   }
 
   @DeleteMapping("/board/{id}")
-  public ResponseEntity<RsData<Object>> deleteBoard(@AuthenticationPrincipal
-  MemberContext memberContext, @PathVariable Long id) {
+  @Operation(summary = "게시물 삭제", description = "게시물을 삭제 처리합니다.")
+  public ResponseEntity<RsData<Object>> deleteBoard(
+      @AuthenticationPrincipal MemberContext memberContext,
+      @Parameter(description = "게시물 ID", required = true, example = "1") @PathVariable Long id) {
     boardService.deleteBoard(memberContext, id);
     return ResponseEntity.ok()
         .body(RsData.of(ResultCode.SUCCESS, "게시물 삭제", null));
   }
 
   @PatchMapping("/board/{id}")
+  @Operation(summary = "게시물 수정", description = "게시물을 수정합니다.")
   public ResponseEntity<RsData<BoardDto>> updateBoard(
-      @AuthenticationPrincipal MemberContext memberContext, @PathVariable Long id,
+      @AuthenticationPrincipal MemberContext memberContext,
+      @Parameter(description = "게시물 ID", required = true, example = "1") @PathVariable Long id,
       @Valid @RequestBody BoardUpdateRequest boardUpdateRequest) {
 
     BoardDto boardDto = boardService.updateBoard(memberContext, id, boardUpdateRequest);
--- JWT_RESTAPI/src/main/java/com/example/jwt_restapi/member/controller/MemberController.java
+++ JWT_RESTAPI/src/main/java/com/example/jwt_restapi/member/controller/MemberController.java
@@ -5,6 +5,8 @@ import com.example.jwt_restapi.base.dto.RsData;
 import com.example.jwt_restapi.member.dto.LoginRequest;
 import com.example.jwt_restapi.member.dto.MemberContext;
 import com.example.jwt_restapi.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/member")
+@Tag(name = "MemberController", description = "로그인 기능, 회원 정보 제공 컨트롤러")
 public class MemberController {
 
   private final MemberService memberService;
 
   @PostMapping("/login")
+  @Operation(summary = "회원 로그인 기능", description = "회원 로그인 시 토큰을 부여합니다.")
   public ResponseEntity<RsData<String>> login(@Valid @RequestBody LoginRequest loginRequest) {
 
     String accessToken = memberService.login(loginRequest);
@@ -33,6 +37,7 @@ public class MemberController {
   }
 
   @GetMapping("/me")
+  @Operation(summary = "회원 정보 제공 기능", description = "로그인한 회원의 정보를 제공합니다.")
   public ResponseEntity<RsData<MemberContext>> findMember(
       @AuthenticationPrincipal MemberContext memberContext) {
 
